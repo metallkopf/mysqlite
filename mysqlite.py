0a33dc9c8573106d6f36f8d0d80465b2cb554eac
metallkopf <47254555+metallkopf@users.noreply.github.com>
2019-08-31 21:32:09 -0400

    cleanup a bit

--- mysqlite.py
+++ mysqlite.py
@@ -3,6 +3,7 @@
 from socketserver import ThreadingTCPServer
 from argparse import ArgumentParser
 from server import Server
+from logging import basicConfig, DEBUG, INFO, info
 
 
 if __name__ == "__main__":
@@ -10,10 +11,15 @@ if __name__ == "__main__":
   parser.add_argument("--path", required=True)
   parser.add_argument("--address", default="localhost")
   parser.add_argument("--port", default=3306)
+  parser.add_argument("--debug", action="store_true")
   args = parser.parse_args()
+
+  level = DEBUG if args.debug else INFO
+  basicConfig(format="%(threadName)s %(levelname).3s %(message)s", level=level)
 
   with ThreadingTCPServer((args.address, args.port), Server, False) as server:
     try:
+      info("STARTING...")
       server.allow_reuse_address = True
       server.server_bind()
       server.server_activate()
@@ -21,4 +27,5 @@ if __name__ == "__main__":
 
       server.serve_forever()
     except KeyboardInterrupt:
+      print("STOPPING...")
       server.shutdown()
--- server.py
+++ server.py
@@ -8,24 +8,22 @@ from database import Database
 from sqlparse import parse
 from definitions import *
 from utils import *
+from logging import debug, info, warning, error
 
 
-PROTOCOL_VERSION = 10
-SERVER_CAPABILITIES = Capability.LONG_PASSWORD | Capability.FOUND_ROWS | \
-  Capability.LONG_FLAG | Capability.NO_SCHEMA | Capability.PROTOCOL_41 | \
-    Capability.SECURE_CONNECTION
-SERVER_CHARSET = Charset.UTF8_GENERAL_CI
-SERVER_STATUS = Status.AUTOCOMMIT
-SERVER_CAPABILITIES2 = 0
+CAPABILITIES = Capability.LONG_PASSWORD | Capability.FOUND_ROWS | \
+  Capability.LONG_FLAG | Capability.CONNECT_WITH_DB | Capability.NO_SCHEMA | \
+  Capability.PROTOCOL_41 | Capability.SECURE_CONNECTION
+CHARSET = Charset.UTF8_GENERAL_CI
+STATUS = Status.AUTOCOMMIT
 
 
 class Server(StreamRequestHandler):
   db = None
   packet_number = -1
-  thread = 0
   connected = False
 
-  client_capabilities = 0
+  capabilities = 0
   max_packet = 0
   charset = 0
   username = ""
@@ -39,39 +37,38 @@ class Server(StreamRequestHandler):
       self.packet_number += 1
     else:
       self.packet_number = 0
-    print(self.thread, "SENDING [%d] (%d)..." % (self.packet_number, len(payload)))
+    info("SENDING [%d] (%d)...", self.packet_number, len(payload))
 
-    header = BytesIO()
-    header.write(pack_integer(len(payload)))
-    header.write(pack_padding())
-    header.write(pack_byte(self.packet_number))
-
-    self.wfile.write(header.getvalue())
+    self.wfile.write(pack_integer(len(payload)))
+    self.wfile.write(pack_padding())
+    self.wfile.write(pack_byte(self.packet_number))
     self.wfile.write(payload)
-    print("", hexlify(payload).decode())
-    print("", payload)
+
+    debug(hexlify(payload).decode())
+    debug(payload)
 
   def send_handshake(self):
-    print(self.thread, "HELLO...")
+    info("HELLO...")
+    thread = int(current_thread().name.split("-")[-1])
 
     payload = BytesIO()
-    payload.write(pack_byte(PROTOCOL_VERSION)) # protocol version
+    payload.write(pack_byte(10)) # protocol version
     payload.write(pack_nullstring(self.db.version)) # server version
-    payload.write(pack_long(self.thread)) # connection id
+    payload.write(pack_long(thread)) # connection id
     payload.write(pack_padding(8)) # auth-plugin-data-part-1
     payload.write(pack_padding()) # filler
-    payload.write(pack_integer(SERVER_CAPABILITIES)) # capability flags
+    payload.write(pack_integer(CAPABILITIES)) # capability flags
 
-    payload.write(pack_byte(SERVER_CHARSET)) # character set
-    payload.write(pack_integer(SERVER_STATUS)) # status flags
-    payload.write(pack_integer(SERVER_CAPABILITIES2 >> 16)) # capability flags
+    payload.write(pack_byte(CHARSET)) # character set
+    payload.write(pack_integer(STATUS)) # status flags
+    payload.write(pack_integer(CAPABILITIES >> 16)) # capability flags
 
     payload.write(pack_padding())
 
     payload.write(pack_padding(10)) # reserved
 
     # auth-plugin-data-part-2
-    if SERVER_CAPABILITIES & Capability.SECURE_CONNECTION:
+    if CAPABILITIES & Capability.SECURE_CONNECTION:
       payload.write(pack_padding(13))
 
     self.send_packet(payload)
@@ -83,8 +80,8 @@ class Server(StreamRequestHandler):
     payload.write(pack_varinteger(affected_rows)) # affected_rows
     payload.write(pack_varinteger(last_insert_id)) # last_insert_id
 
-    if SERVER_CAPABILITIES & Capability.PROTOCOL_41:
-      payload.write(pack_integer(SERVER_STATUS)) # status_flags
+    if CAPABILITIES & Capability.PROTOCOL_41:
+      payload.write(pack_integer(STATUS)) # status_flags
       payload.write(pack_integer(warnings)) # warnings
 
     self.send_packet(payload)
@@ -93,7 +90,7 @@ class Server(StreamRequestHandler):
     payload = BytesIO()
     payload.write(pack_byte(0xfe)) # header
     payload.write(pack_integer(warnings)) # warnings
-    payload.write(pack_integer(SERVER_STATUS))
+    payload.write(pack_integer(STATUS))
 
     self.send_packet(payload)
 
@@ -102,7 +99,7 @@ class Server(StreamRequestHandler):
     payload.write(pack_byte(0xff)) # header
     payload.write(pack_integer(error)) # error_code
 
-    if SERVER_CAPABILITIES & Capability.PROTOCOL_41:
+    if self.capabilities & Capability.PROTOCOL_41:
       payload.write(pack_fixedstring("#")) # sql_state_marker
       payload.write(pack_fixedstring("42000")) # sql_state
 
@@ -110,15 +107,15 @@ class Server(StreamRequestHandler):
 
     self.send_packet(payload)
 
-  def send_columndef(self, name, length, field, table=""):
+  def send_columndef(self, name, length, field):
     payload = BytesIO()
 
     decimals = 0 if field == FieldType.LONGLONG else 0x1f
     collation = Charset.BINARY if field == FieldType.LONGLONG else Charset.UTF8_GENERAL_CI
 
     payload.write(pack_string("def")) # catalog
-    payload.write(pack_string(self.schema)) # schema
-    payload.write(pack_string(table)) # table
+    payload.write(pack_string("")) # schema
+    payload.write(pack_string("")) # table
 
     payload.write(pack_padding()) # org_table
 
@@ -157,9 +154,29 @@ class Server(StreamRequestHandler):
 
     self.send_eof()
 
+  def handle_handshake(self, payload):
+    self.capabilities, self.max_packet, self.charset = \
+      read_data(payload, "<IIb23x")
+    self.username = read_string(payload)
+
+    if self.capabilities & Capability.SECURE_CONNECTION:
+      length = read_data(payload, "<b")[0]
+      read_data(payload, "%ds" % length)[0]
+    else:
+      read_string(payload)
+
+    if self.capabilities & Capability.CONNECT_WITH_DB:
+      self.schema = read_string(payload)
+
+    if True:
+      self.send_ok()
+      self.connected = True
+    else:
+      pass
+
+
   def handle(self):
     self.db = Database(self.server.path)
-    self.thread = int(current_thread().name.split("-")[-1])
     self.send_handshake()
 
     while True:
@@ -167,32 +184,22 @@ class Server(StreamRequestHandler):
       self.packet_number = header >> 24
       size = self.packet_number << 24 ^ header
 
-      print(self.thread, "RECEIVING [%d] (%d)..." % (self.packet_number, size))
+      info("RECEIVING [%d] (%d)...", self.packet_number, size)
       payload = BytesIO()
       payload.write(self.rfile.read(size))
       payload.seek(0)
 
-      print("", hexlify(payload.getvalue()).decode())
-      print("", payload.getvalue())
+      debug(hexlify(payload.getvalue()).decode())
+      debug(payload.getvalue())
 
       if not self.connected:
-        self.client_capabilities, self.max_packet, self.charset = \
-          read_data(payload, "<IIb23x")
-        self.username = read_string(payload)
-        #length = read_data(payload, "<b")[0]
-        #auth_response = read_data(payload, "%ds" % length)[0]
-
-        if True:
-          self.send_ok()
-          self.connected = True
-        else:
-          pass
+        self.handle_handshake(payload)
       else:
         command = read_data(payload, "<B")[0]
 
         if command == Command.QUERY:
           query = read_string(payload).strip().strip(";")
-          print(self.thread, "QUERY:", query)
+          info("QUERY: %s", query)
 
           if query.upper() == "SHOW DATABASES":
             self.send_resultset(self.db.show_databases())
@@ -225,7 +232,7 @@ class Server(StreamRequestHandler):
             self.send_ok()
           elif query.upper().startswith("USE"):
             self.schema = query.split(" ")[-1].strip("`")
-            print("USE DATABASE", self.schema)
+            info("USE DATABASE %s", self.schema)
             self.send_ok()
           elif query.upper().startswith("CREATE") or \
               query.upper().startswith("ALTER") or \
@@ -252,13 +259,13 @@ class Server(StreamRequestHandler):
               self.send_error(str(e))
         elif command == Command.INIT_DB:
           self.schema = read_string(payload)
-          print("USE DATABASE", self.schema)
+          info("USE DATABASE %s", self.schema)
           self.send_ok()
         elif command == Command.QUIT:
-          print(self.thread, "BYE...")
+          info("BYE...")
           break
         elif command == Command.PING:
-          print(self.thread, "PING...")
+          info("PING...")
           self.send_ok()
         else:
           self.send_error()
